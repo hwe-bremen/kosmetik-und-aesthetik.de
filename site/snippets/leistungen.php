4013b6911f87be07237139a9da65523d83cb2380
Hans-Werner <plan2-hw@dobben-united.de>
2021-03-03 18:01:48 +0100

    03-01

--- site/snippets/leistungen.php
+++ site/snippets/leistungen.php
@@ -1,9 +1,23 @@
 <section id="leistungen">
+<div class="section_leistungen-intro">
     <div class="section_leistungen-text"> 
-   
-    <?= $data->text()->myBlocksField()->toBlocks() ?>
-    </div>      
-  
+        <?= $data->text()->myBlocksField()->toBlocks() ?>
+    </div>
+</div>
+
+
+<div class="section_leistungen-cards"> 
+
+<?php foreach($data->children()->listed() as $leistungen): ?>
+    <div class="section_leistungen-card">
+            <h2>    <?= $leistungen->title() ?></h2> <br>
+            <p><?= $leistungen->text() ?> </p> 
+    </div>
+    <?php endforeach ?>
+</div>
+
+
+
 </section> 
      
 
